afe0af9b2e62d6c0114f47c015c8636ed1ade986
Suraj Shrestha <syuraj@gmail.com>
2021-07-12 22:06:29 -0400

    first commit: basic spring app

---
+++ src/main/java/com/tradiumapp/swingtradealerts/SwingTradeAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.tradiumapp.swingtradealerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingTradeAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwingTradeAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradiumapp/swingtradealerts/SwingTradeAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradiumapp.swingtradealerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingTradeAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
