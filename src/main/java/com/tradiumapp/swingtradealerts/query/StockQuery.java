c8656d2cad08a423ac4919ae3522a1af99911d55
Suraj Shrestha <syuraj@gmail.com>
2021-07-20 01:13:02 -0400

    support getWatchList

--- src/main/java/com/tradiumapp/swingtradealerts/query/StockQuery.java
+++ src/main/java/com/tradiumapp/swingtradealerts/query/StockQuery.java
@@ -2,28 +2,26 @@ package com.tradiumapp.swingtradealerts.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.tradiumapp.swingtradealerts.models.Stock;
-import com.tradiumapp.swingtradealerts.models.User;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Component
-public class UserQuery implements GraphQLQueryResolver {
+public class StockQuery implements GraphQLQueryResolver {
     @Autowired
     MongoTemplate mongoTemplate;
 
-    public List<Stock> getWatchList() {
-        Query query1 = new Query();
-        query1.addCriteria(Criteria.where("firebaseUid").is("6V7jHPpxTeXfUMI39HtIRGEMVv13"));
-        User user = mongoTemplate.findOne(query1, User.class);
+    public List<Stock> searchStocks(String searchTerm) {
+        Query query = new Query();
+        query.addCriteria(new Criteria().orOperator(
+                Criteria.where("symbol").regex(searchTerm, "gmi"),
+                Criteria.where("company").regex(searchTerm, "gmi")));
 
-
-        return user.watchList;
+        List<Stock> stocks = mongoTemplate.find(query, Stock.class);
+        return stocks;
     }
 }
