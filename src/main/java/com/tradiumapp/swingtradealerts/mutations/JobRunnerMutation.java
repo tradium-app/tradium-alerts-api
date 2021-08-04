850000d1a353b3d750931a8d298c616d72eb6f2f
Suraj Shrestha <syuraj@gmail.com>
2021-08-03 22:37:54 -0400

    add graphql api to run job for now

---
+++ src/main/java/com/tradiumapp/swingtradealerts/mutations/JobRunnerMutation.java
@@ -0,0 +1,32 @@
+package com.tradiumapp.swingtradealerts.mutations;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.tradiumapp.swingtradealerts.models.Response;
+import com.tradiumapp.swingtradealerts.scheduledtasks.FetchAllStocksTask;
+import com.tradiumapp.swingtradealerts.scheduledtasks.FetchQuotesTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JobRunnerMutation implements GraphQLMutationResolver {
+    Logger logger = LoggerFactory.getLogger(AlertMutation.class);
+
+    @Autowired
+    private FetchAllStocksTask fetchAllStocksTask;
+
+    @Autowired
+    private FetchQuotesTask fetchQuotesTask;
+
+    public Response runJob(final float jobId) throws IOException {
+        if(jobId == 1001) fetchAllStocksTask.fetchAllStocks();
+        else if(jobId == 1002) fetchQuotesTask.fetchQuotes();
+
+        logger.info("Job {} ran successfully.", jobId);
+
+        return new Response(true, String.format("Job %s ran successfully.",jobId));
+    }
+}
