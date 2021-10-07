6c38e5aa55c4107498445663f4f98a50aca9082c
Suraj Shrestha <syuraj@gmail.com>
2021-10-07 19:07:19 -0400

    A unit test using embedded mongo; simple alert test with 52Week High condition

--- src/test/java/com/tradiumapp/swingtradealerts/scheduledtasks/SendAlertTask_52WeekHigh_Condition_Test.groovy
+++ src/test/java/com/tradiumapp/swingtradealerts/scheduledtasks/SendAlertTask_52WeekHigh_Condition_Test.groovy
@@ -1,83 +1,87 @@
 package com.tradiumapp.swingtradealerts.scheduledtasks
 
-import com.mongodb.client.result.UpdateResult
 import com.tradiumapp.swingtradealerts.models.*
 import com.tradiumapp.swingtradealerts.repositories.AlertRepository
+import com.tradiumapp.swingtradealerts.repositories.StockHistoryRepository
 import com.tradiumapp.swingtradealerts.repositories.StockRepository
 import com.tradiumapp.swingtradealerts.repositories.UserRepository
-import com.tradiumapp.swingtradealerts.scheduledtasks.helpers.SendGridEmailSender
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
-import org.springframework.data.mongodb.core.MongoTemplate
+import com.tradiumapp.swingtradealerts.scheduledtasks.helpers.AlertEmailSender
+import org.mockito.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
+import org.testng.Assert
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 
 import java.time.Instant
 
 import static org.mockito.ArgumentMatchers.any
-import static org.mockito.ArgumentMatchers.eq
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.when
+import static org.mockito.Mockito.verify
 
-class SendAlertTaskTest extends AbstractTestNGSpringContextTests {
-    @Mock
+@DataMongoTest
+class SendAlertTask_52WeekHigh_Condition_Test extends AbstractTestNGSpringContextTests {
+    @Autowired
     private AlertRepository alertRepository;
 
-    @Mock
+    @Autowired
     private UserRepository userRepository;
 
-    @Mock
+    @Autowired
     private StockRepository stockRepository;
 
-    @Mock
-    MongoTemplate mongoTemplate;
+    @Autowired
+    private StockHistoryRepository stockHistoryRepository;
 
     @Mock
-    SendGridEmailSender emailSender;
+    AlertEmailSender alertEmailSender;
 
+    @Autowired
     @InjectMocks
     private SendAlertTask task
+
+    User testUser = new User();
 
     @BeforeClass
     public void setUp() {
         MockitoAnnotations.initMocks(this)
 
+        testUser.email = "test@test.com"
+        userRepository.save(testUser);
+
         Alert testAlert = new Alert()
         testAlert.title = "test alert"
         testAlert.symbol = "TEST"
-
+        testAlert.userId = testUser.id
+        testAlert.status = Alert.AlertStatus.Off
         def config = new Condition.ValueConfig()
         config.value = 20;
         def condition = new Condition(IndicatorType.week52high, "20_below_week52high", config)
         testAlert.conditions = Arrays.asList(condition)
-
-        List<Alert> alerts = Arrays.asList(testAlert)
-
-        when(alertRepository.findByStatusNot(Alert.AlertStatus.Disabled)).thenReturn(alerts)
+        alertRepository.save(testAlert);
 
         StockHistory stockHistory = new StockHistory()
         stockHistory.symbol = testAlert.symbol;
         stockHistory.daily_priceHistory = new ArrayList<>()
-
         def price = new StockHistory.StockPrice()
         price.close = 75;
         price.time = Instant.now().toEpochMilli()
-
         stockHistory.daily_priceHistory.add(price)
-        when(mongoTemplate.findOne(any(), eq(StockHistory.class))).thenReturn(stockHistory)
+        stockHistoryRepository.save(stockHistory)
 
         Stock stock = new Stock()
+        stock.symbol = testAlert.symbol;
         stock.week52High = 100;
-        when(stockRepository.findBySymbol(testAlert.symbol)).thenReturn(stock)
-
-        def result = mock(UpdateResult.class)
-        when(mongoTemplate.updateFirst(any(), any(), eq(Alert.class) as Class)).thenReturn(result)
+        stockRepository.save(stock)
     }
+
+    @Captor
+    ArgumentCaptor<User> userCaptor;
 
     @Test(groups = "unit")
     void testSendAlerts() {
         task.sendAlerts();
+        verify(alertEmailSender).sendEmail(userCaptor.capture(), any())
+        Assert.assertEquals(userCaptor.getValue().email, testUser.email)
     }
 }
