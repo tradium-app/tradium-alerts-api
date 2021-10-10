241e65eccf089fd0780689c81772cf5218b51e92
Suraj Shrestha <syuraj@gmail.com>
2021-10-10 00:23:25 -0400

    minor: support multiple origins; helps local react testing against heroku server

--- src/main/java/com/tradiumapp/swingtradealerts/auth/SimpleCorsFilter.java
+++ src/main/java/com/tradiumapp/swingtradealerts/auth/SimpleCorsFilter.java
@@ -6,26 +6,31 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCorsFilter extends OncePerRequestFilter {
 
     @Value("${graphql.servlet.cors.allowedOrigins}")
-    private String allowedOrigins;
+    private List<String> allowedOrigins;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        response.setHeader("Access-Control-Allow-Origin", allowedOrigins);
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "12000");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-authorization-tradium");
-        response.setHeader("Access-Control-Expose-Headers", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        String origin = request.getHeader("origin");
+        if (allowedOrigins.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            response.setHeader("Access-Control-Max-Age", "12000");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, x-authorization-tradium");
+            response.setHeader("Access-Control-Expose-Headers", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+        }
 
         if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_OK);
